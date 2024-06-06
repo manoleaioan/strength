d69cb6cf2025d56dc3233c946f36dfebe2479716
ioan <ioanmanolea@gmail.com>
2024-06-06 20:19:25 +0300

    general updates

--- frontend/src/components/BgAnimation/BgAnimation.js
+++ frontend/src/components/BgAnimation/BgAnimation.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { ReactComponent as LeftShape } from '../../assets/LeftShape.svg';
+import { ReactComponent as RightShape } from '../../assets/RightShape.svg';
+
 import "./BgAnimation.scss";
 
 export default function BgAnimation() {
   return (
     <div className='bg-shapes'>
-      <div className="left-shape" ></div>
-      <div className="right-shape" ></div>
+      <LeftShape className="left-shape" />
+      <RightShape className="right-shape" />
 
-      {/* <ul className="circles">
+      <ul className="circles">
         <li></li> <li></li><li></li><li></li><li></li><li></li> <li></li><li></li><li></li> <li></li>
-      </ul> */}
+      </ul>
     </div>
   )
 }
--- frontend/src/pages/Account/Account.js
+++ frontend/src/pages/Account/Account.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate , useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./Account.scss";
 
@@ -71,7 +71,7 @@ export default function Account() {
 
       </motion.div>
 
-      {/* <BgAnimation /> */}
+      <BgAnimation />
     </div>
   )
 }
